80d33530af0a3b07362f3d86a423b54e2b982045
Lxc <mel+lxc@ennder.fr>
2020-01-31 15:23:00 +0100

    V1.0.1 Readme updated
    modifié :         README.md
    modifié :         init.rb

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ No tests have been implemented so far
 
 Thanks to XDSoft for JS DateTime Picker :
 https://xdsoft.net/jqplugins/datetimepicker/
+
+# TODO
+
+Merge with https://github.com/nanego/redmine_datetime_custom_field ?
--- init.rb
+++ init.rb
@@ -7,7 +7,8 @@ Redmine::Plugin.register :redmine_datetime_custom_field do
   name 'Redmine Datetime Custom Field plugin'
   author 'Anthony LEGIRET <anthony.legiret@smile.fr>, Jérôme BATAILLE <jerome.bataille@smile.fr>'
   description 'This plugin adds the DateTime type to Redmine custom fields'
-  version '1.0.0'
+  url "https://github.com/Smile-SA/redmine_datetime_custom_field"
+  version '1.0.1'
   requires_redmine :version_or_higher => '3.1.1'
 end
 
