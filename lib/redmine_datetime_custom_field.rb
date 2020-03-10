e575ff4671eef6206cced9bf7d13f6b4aa85a636
Vegaminer <vegaminer@cryptofarm.wiki>
2020-03-11 02:49:27 +0300

    More code refactoring

--- lib/redmine_datetime_custom_field.rb
+++ lib/redmine_datetime_custom_field.rb
@@ -2,9 +2,9 @@
 Rails.configuration.to_prepare do
   require 'redmine_datetime_custom_field/hooks/hooks'
   
-  require 'datetime_custom_field_application_helper_patch'
-  require 'datetime_custom_field_field_format_patch'
-  require 'datetime_custom_field_custom_fields_helper_patch'
+  require 'redmine_datetime_custom_field/patches/application_helper_patch'
+  require 'redmine_datetime_custom_field/patches/field_format_patch'
+  require 'redmine_datetime_custom_field/patches/custom_fields_helper_patch'
 
   require 'datetime_custom_field_query_patch'
   Query.send(:prepend, DatetimeCustomFieldQueryPatch)
