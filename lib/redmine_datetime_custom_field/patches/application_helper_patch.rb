0d63f041c7252c427662863efd92f6d192e6c7c8
Vegaminer <vegaminer@cryptofarm.wiki>
2020-04-30 19:10:59 +0300

    Adding datetime_js_code_for and ability to pass callbacks as option properties

--- lib/redmine_datetime_custom_field/patches/application_helper_patch.rb
+++ lib/redmine_datetime_custom_field/patches/application_helper_patch.rb
@@ -1,42 +1,60 @@
 require_dependency 'application_helper'
 
 module ApplicationHelper
-  ###################
-  # Plugin new method
-  def datetime_for( field_id, aOptions = {} )
-    options = getDatetimepickerOptions( aOptions )
+
+    #** datetime_for
+    def datetime_for( field_id, aOptions = {} )
+        tag = "\n".html_safe + javascript_tag( datetime_js_code_for( field_id, aOptions ) )
+    end
     
-    a = "\n".html_safe +
-      javascript_tag(
-        "$(function() { " +
-        "$('##{field_id}').datetimepicker(#{options}); " +
-        "});"
-      )
-    a
-  end
+    #** datetime_js_code_for
+    def datetime_js_code_for( field_id, aOptions = {} )
+        options = datetime_options( aOptions )    
+        code = "$(function() { $('##{field_id}').datetimepicker(#{options}); });".html_safe
+    end
+  
+    #** datetime_options
+    def datetime_options( aOptions = {} )
+        return aOptions if aOptions.class.name == 'String' 
+        
+        start_of_week = Setting.start_of_week
+        start_of_week = l(:general_first_day_of_week, :default => '1') if start_of_week.blank?
 
-  ###################
-  # Plugin new method
-  def getDatetimepickerOptions( aOptions = {} )
-    start_of_week = Setting.start_of_week
-    start_of_week = l(:general_first_day_of_week, :default => '1') if start_of_week.blank?
+        # Redmine uses 1..7 (monday..sunday) in settings and locales
+        # JQuery uses 0..6 (sunday..saturday), 7 needs to be changed to 0
+        start_of_week = start_of_week.to_i % 7
 
-    # Redmine uses 1..7 (monday..sunday) in settings and locales
-    # JQuery uses 0..6 (sunday..saturday), 7 needs to be changed to 0
-    start_of_week = start_of_week.to_i % 7
-
-    jquery_locale = l( 'jquery.locale', :default => current_language.to_s )
-    options = {
-        'format' => 'Y-m-d H:i',
-        'dayOfWeekStart' => start_of_week,
-        'closeOnDateSelect' => true,
-        'lang' => jquery_locale
-    }.merge( aOptions )
-    
-    options = ActiveSupport::JSON.encode( options )
-    puts options.inspect
-    options
-  end
+        jquery_locale = l( 'jquery.locale', :default => current_language.to_s )
+        options = {
+            'format' => 'Y-m-d H:i',
+            'dayOfWeekStart' => start_of_week,
+            'closeOnDateSelect' => true,
+            'lang' => jquery_locale
+        }.merge( aOptions )
+        
+        # puts "UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU 0:" + options.inspect
+        # Handle events in options. We have to pass it as js-code, not as json-string
+        events = []
+        [ :onSelectDate, :onSelectTime, :onChangeMonth, :onChangeYear, :onChangeDateTime, :onShow, :onClose, :onGenerate ].each {|e|
+            has_symbol = options.has_key?(e)
+            has_string = !has_symbol && options.has_key?( e.to_s )
+            next unless ( has_symbol || has_string )
+            e = e.to_s if has_string
+            
+            # puts e
+            events.push( e.to_s + ':' + options[e] )
+            options.delete(e)
+        }
+        # puts "UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU 1: " + events.inspect
+        
+        options = ActiveSupport::JSON.encode( options )
+        if events.count > 0 
+            options = options.sub( /}$/, ', ' + events.join( ',' ) + '}' )
+        end
+        
+        # puts options.inspect
+        options
+    end
   
 =begin
   ###################
